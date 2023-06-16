046041397496a349f7846a23b0e17e997aba20e1
Arunachalam <arunKumaran0133@gmail.com>
2023-06-16 07:07:33 +0000

    Date Added

---
+++ index.js
@@ -0,0 +1,9 @@
+var addDays = require("date-fns/addDays");
+
+const getDateAfterxDays = (days) => {
+  const newDate = addDays(new Date(2020, 7, 22), days);
+  return `${newDate.getDate()}-${
+    newDate.getMonth() + 1
+  }-${newDate.getFullYear()}`;
+};
+module.exports = getDateAfterxDays;
